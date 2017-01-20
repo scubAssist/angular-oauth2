f46c1b9235fc59c7eaa8ccad28fd5f5b65a1ce17
scubAssist <info@scubassist.com>
2017-01-20 09:29:05 +0100

    WSO specific alterations
    WSO does not conform to the standards...

--- src/interceptors/oauth-interceptor.js
+++ src/interceptors/oauth-interceptor.js
@@ -20,17 +20,20 @@ function oauthInterceptor($q, $rootScope, OAuthToken) {
       if (400 === rejection.status && rejection.data &&
         ('invalid_request' === rejection.data.error || 'invalid_grant' === rejection.data.error)
       ) {
-        OAuthToken.removeToken();
+        // Do NOT remove the token on 400, WSO sometimes throws this...
+        //OAuthToken.removeToken();
 
         $rootScope.$emit('oauth:error', rejection);
       }
 
+      // WSO does not specify the rejection reason, just the global 401...
       // Catch `invalid_token` and `unauthorized` errors.
       // The token isn't removed here so it can be refreshed when the `invalid_token` error occurs.
-      if (401 === rejection.status &&
-        (rejection.data && 'invalid_token' === rejection.data.error) ||
-        (rejection.headers('www-authenticate') && 0 === rejection.headers('www-authenticate').indexOf('Bearer'))
-      ) {
+//       if (401 === rejection.status &&
+//         (rejection.data && 'invalid_token' === rejection.data.error) ||
+//         (rejection.headers('www-authenticate') && 0 === rejection.headers('www-authenticate').indexOf('Bearer'))
+//       ) {
+      if (401 === rejection.status) {
         $rootScope.$emit('oauth:error', rejection);
       }
 
